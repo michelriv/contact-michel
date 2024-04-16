a2766d0fa26a6cd662a3a165cb7eed23b6cb4db0
Michel Rivero <157402536+michelriv@users.noreply.github.com>
2024-04-16 20:19:01 +0000

    not done

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,40 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	let apiUrl = "https://playground.4geeks.com/contact/agendas/michelriv"
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: []
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			findOrCreateAgenda: async()=>{
+				let response = await fetch(apiUrl)
+				if (response.status == 404) {
+					fetch(apiUrl, {
+						method: "POST", 
+						"Content-Type": "application/json"
+					})
+					.then(resp=>{
+						console.log(resp.json())
+						getActions().getContacts()
+					}
+					
+				)
+				.catch(error=>console.log(error))
+				}
+				else{
+					getActions().getContacts()
+				}
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			getContacts: ()=>{
+				fetch(apiUrl+"/contacts")
+				.then(resp=>resp.json())
+				.then(data=>setStore({contacts:data.contacts}))
+				.catch(error=>console.log(error))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			addContacts: ()=>{},
+			editContacts: ()=>{},
+			deleteContacts: ()=>{},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
